f28c81d0fffa0b535c7716d55737f7152eea5522
MaxwellWibert <xwibert@gmail.com>
2017-06-04 14:21:00 -0700

    one more time, come on heroku

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,43 +1,42 @@
-var express = require("express");
 var db = require("../models")
 
-var router = express.Router();
-
-router.get("/api", (req, res) => {
-	db.Burger.findAll().then(dbBurger=> {res.json(dbBurger)});
-});
-
-router.post("/api", (req, res)=>{
-	db.Burger.create(req.body).then(dbBurger=>{
-		res.redirect("/");
+var router = function(app){
+	app.get("/api", (req, res) => {
+		db.Burger.findAll().then(dbBurger=> {res.json(dbBurger)});
 	});
-});
 
-router.put("/api/:id", (req, res) =>{
-
-	db.Burger.update(req.body , {
-		where: {
-			id: req.params.id
-		}
-	}).then(dbBurger => res.redirect("/"));
-});
-console.log("++++++++++++++++++++++++");
-router.delete("/api/:id", (req, res)=>{
-	db.Burger.destroy({
-		where: {
-			id: req.params.id
-		}
-	}).then(dbBurger => res.redirect("/"));
-});
-
-router.get("/", (req, res)=>{
-
-	db.Burger.findAll({}).then(dbBurger=>{
-		var hbsObject = {
-			burgers: dbBurger
-		};
-		res.render("index", hbsObject);
+	app.post("/api", (req, res)=>{
+		db.Burger.create(req.body).then(dbBurger=>{
+			res.redirect("/");
+		});
 	});
-});
+
+	app.put("/api/:id", (req, res) =>{
+
+		db.Burger.update(req.body , {
+			where: {
+				id: req.params.id
+			}
+		}).then(dbBurger => res.redirect("/"));
+	});
+	console.log("++++++++++++++++++++++++");
+	app.delete("/api/:id", (req, res)=>{
+		db.Burger.destroy({
+			where: {
+				id: req.params.id
+			}
+		}).then(dbBurger => res.redirect("/"));
+	});
+
+	app.get("/", (req, res)=>{
+
+		db.Burger.findAll({}).then(dbBurger=>{
+			var hbsObject = {
+				burgers: dbBurger
+			};
+			res.render("index", hbsObject);
+		});
+	});
+}
 
 module.exports = router;
--- server.js
+++ server.js
@@ -9,13 +9,12 @@ var app = express();
 // Requiring our models for syncing
 var db = require("./models");
 
-var PORT = 3306;
+var PORT = process.env.PORT || 8000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.use(methodOverride("_method"));
 // Static directory
@@ -25,14 +24,14 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // Routes =============================================================
 
-var routes = require("./controllers/burgers_controller.js");
-
-app.use("/", routes);
+require("./controllers/burgers_controller.js")(app);
 console.log("routes used");
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync().then(function() {
-  app.listen(3306, function() {
+  app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-});
+}).catch(err=>{
+	console.error(err);
+})
 
