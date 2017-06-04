befef32fc4a8c67565ccd2aa993910a844c5c170
MaxwellWibert <xwibert@gmail.com>
2017-06-04 14:47:30 -0700

    changed index.js file configuration

--- server.js
+++ server.js
@@ -13,8 +13,9 @@ var PORT = process.env.PORT || 8000;
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
 
 app.use(methodOverride("_method"));
 // Static directory
