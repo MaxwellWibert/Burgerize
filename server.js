c4ee12c0da25dd4c611ca052588b3f2cfe7a0acb
MaxwellWibert <xwibert@gmail.com>
2017-06-04 14:58:14 -0700

    will try anything at this point

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ require("./controllers/burgers_controller.js")(app);
 console.log("routes used");
 // Syncing our sequelize models and then starting our express app
 db.sequelize.sync().then(function() {
+	console.log("/////////////////////////////////////////////////////////////////////////////////");
+	console.log("PORT: " + PORT);
+	console.log("/////////////////////////////////////////////////////////////////////////////////");
+}
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
